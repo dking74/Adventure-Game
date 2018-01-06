e3fdec4153afd3d2249e6761eeb849fa62c43dc8
Paul Gross <pgross@maryville.edu>
2018-01-06 11:46:10 -0600

    Initial commit

---
+++ adventure_game.py
@@ -0,0 +1,7 @@
+# You are welcome to write and include any other Python files you want or need
+# however your game must be started by calling the main function in this file.
+
+def main():
+    print('YOUR GAME SHOULD BE STARTED HERE. ALSO REMOVE THIS.')
+    
+main()
